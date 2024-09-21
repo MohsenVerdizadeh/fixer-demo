db50373309fd7ee26fdf42e4ee3b727c141ed077
mohsen <mohsenverdizadehkohi@gmail.com>
2024-09-21 16:35:26 +0330

    jump to version 0.2 of fixer

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,16 @@
-def get_rates():
-    print('Get rates touched.')
+import requests
+import json
+
+base_address = "http://data.fixer.io/api/latest?access_key="
+
+
+def get_rates(api_key):
+    """
+    send a get request to fixer.io's Api and get rates and transform it to dict and return it
+    :return: dict of get request's response
+    """
+    response = requests.get(base_address + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        return None
--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='fixer_demo',
-    version='0.1',
+    version='0.2',
     description="Fixer Service demo packages",
-    url="#",
+    url="https://github.com/MohsenVerdizadeh/fixer-demo.git",
     author="mohsen",
     author_email='mohsenverdizadehkohi@gmail.com',
     license='MIT',
     packages=['fixer'],
+    install_requiers=['requests'],
     zip_safe=False
 )
