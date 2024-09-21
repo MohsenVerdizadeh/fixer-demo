3cf13f0538b76d5f201413ce691db5334e9f1a2f
mohsen <mohsenverdizadehkohi@gmail.com>
2024-09-21 16:10:29 +0330

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fixer_demo',
+    version='0.1',
+    description="Fixer Service demo packages",
+    url="#",
+    author="mohsen",
+    author_email='mohsenverdizadehkohi@gmail.com',
+    license='MIT',
+    packages=['fixer'],
+    zip_safe=False
+)
